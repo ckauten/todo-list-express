fe68b9b4ec8255af727d4894e7873630a25e5191
Cameron Kauten <cameron.kauten@gmail.com>
2024-06-12 10:49:18 -0700

    commented out all lines of code

--- server.js
+++ server.js
@@ -1,49 +1,54 @@
-const express = require("express");
+const express = require('express');
+//brings in express to be used
 const app = express();
-const MongoClient = require("mongodb").MongoClient;
+//sets express to an app variable
+const MongoClient = require('mongodb').MongoClient;
+//brings in the mongo client to be used
 const PORT = 2121;
-require("dotenv").config();
+//sets the port to 2121
+require('dotenv').config();
+//allows dotenv to be used in the config file
 
 let db,
   dbConnectionStr = process.env.DB_STRING,
-  dbName = "todo";
+  dbName = 'todo';
+//sets the db, dbConnectionStr to process the env db_string, and sets the dbName to todo
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then((client) => {
+  //connects to the dbConnectionStr and uses the unified topology (which is new i guess)
   console.log(`Connected to ${dbName} Database`);
+  //console log to let us know we are connectd to the database
   db = client.db(dbName);
+  //establishes a connection to a speacific database and assigns it as the db variable
 });
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
+//sets the view engine to ejs
+app.use(express.static('public'));
+//allows the use of the public folder
 app.use(express.urlencoded({ extended: true }));
+//allows the use of urlencoded data which is useful because it allows us to send data to the server
 app.use(express.json());
+//allows the use of json data
 
-app.get("/", async (request, response) => {
-  const todoItems = await db.collection("todos").find().toArray();
-  const itemsLeft = await db.collection("todos").countDocuments({ completed: false });
-  response.render("index.ejs", { items: todoItems, left: itemsLeft });
-  // db.collection('todos').find().toArray()
-  // .then(data => {
-  //     db.collection('todos').countDocuments({completed: false})
-  //     .then(itemsLeft => {
-  //         response.render('index.ejs', { items: data, left: itemsLeft })
-  //     })
-  // })
-  // .catch(error => console.error(error))
+app.get('/', async (request, response) => {
+  const todoItems = await db.collection('todos').find().toArray();
+  const itemsLeft = await db.collection('todos').countDocuments({ completed: false });
+  response.render('index.ejs', { items: todoItems, left: itemsLeft });
 });
 
-app.post("/addTodo", (request, response) => {
-  db.collection("todos")
+app.post('/addTodo', (request, response) => {
+  db.collection('todos')
     .insertOne({ thing: request.body.todoItem, completed: false })
     .then((result) => {
-      console.log("Todo Added");
-      response.redirect("/");
+      console.log('Todo Added');
+      response.redirect('/');
     })
     .catch((error) => console.error(error));
 });
 
-app.put("/markComplete", (request, response) => {
-  db.collection("todos")
+app.put('/markComplete', (request, response) => {
+  db.collection('todos')
     .updateOne(
       { thing: request.body.itemFromJS },
       {
@@ -57,14 +62,14 @@ app.put("/markComplete", (request, response) => {
       }
     )
     .then((result) => {
-      console.log("Marked Complete");
-      response.json("Marked Complete");
+      console.log('Marked Complete');
+      response.json('Marked Complete');
     })
     .catch((error) => console.error(error));
 });
 
-app.put("/markUnComplete", (request, response) => {
-  db.collection("todos")
+app.put('/markUnComplete', (request, response) => {
+  db.collection('todos')
     .updateOne(
       { thing: request.body.itemFromJS },
       {
@@ -78,18 +83,18 @@ app.put("/markUnComplete", (request, response) => {
       }
     )
     .then((result) => {
-      console.log("Marked Complete");
-      response.json("Marked Complete");
+      console.log('Marked Complete');
+      response.json('Marked Complete');
     })
     .catch((error) => console.error(error));
 });
 
-app.delete("/deleteItem", (request, response) => {
-  db.collection("todos")
+app.delete('/deleteItem', (request, response) => {
+  db.collection('todos')
     .deleteOne({ thing: request.body.itemFromJS })
     .then((result) => {
-      console.log("Todo Deleted");
-      response.json("Todo Deleted");
+      console.log('Todo Deleted');
+      response.json('Todo Deleted');
     })
     .catch((error) => console.error(error));
 });
